bcbe961a6f4407f3d71d51d9e7648a8c4d11619c
rohankhan009 <r80398232@gmail.com>
2026-01-20 21:16:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // --- Admin Configuration ---
-const MY_ADMIN_KEY = "lodalelo@1122330"; // App login ke liye yahi use karein
+const MY_ADMIN_KEY = "lodalelo@11233"; // App login ke liye yahi use karein
 
 // 1. REGISTER / LOGIN ENDPOINT
 app.post('/register', (req, res) => {
